8f1ef2aa701286bccd6071b972fac23bfb8a05fc
Roman <pavlyukroman9@gmail.com>
2025-08-27 13:30:36 +0300

    Add Task-3 Form

---
+++ task-3/task-3.js
@@ -0,0 +1,27 @@
+document.querySelector("#send").onclick = function() {
+  let country = document.querySelector("#country").value;
+  let url = "http://universities.hipolabs.com/search?country=" + country;
+
+  fetch(url)
+    .then(r => r.json())
+    .then(data => {
+      let table = "<table border='1'><tr><th>№</th><th>Name</th><th>Country</th><th>Code</th><th>Domains</th><th>Website</th></tr>";
+      for (let i=0; i<data.length; i++) {
+        table += "<tr>";
+        table += "<td>"+(i+1)+"</td>";
+        table += "<td>"+data[i].name+"</td>";
+        table += "<td>"+data[i].country+"</td>";
+        table += "<td>"+data[i].alpha_two_code+"</td>";
+        table += "<td>"+data[i].domains.join(", ")+"</td>";
+        table += "<td><a href='"+data[i].web_pages[0]+"' target='_blank'>Сайт</a></td>";
+        table += "</tr>";
+      }
+      table += "</table>";
+      document.querySelector("#results").innerHTML = table;
+    })
+}
+
+document.querySelector("#reset").onclick = function() {
+  document.querySelector("#country").value = "";
+  document.querySelector("#results").innerHTML = "";
+}
